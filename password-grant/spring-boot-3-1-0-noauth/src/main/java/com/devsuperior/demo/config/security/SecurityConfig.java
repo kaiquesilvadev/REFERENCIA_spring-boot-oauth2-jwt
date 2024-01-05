f4658d2cbddf028c4afa65c55d34934949c09bf1
kaiquesilvadev <j.kaique1570@Gmail.com>
2024-01-04 21:01:27 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/security/SecurityConfig.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/security/SecurityConfig.java
@@ -3,11 +3,26 @@ package com.devsuperior.demo.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+	/**
+	 * Configuração do Bean para fornecer um objeto BCryptPasswordEncoder.
+	 * O BCryptPasswordEncoder é um algoritmo de hash de senhas usado para criptografar e verificar senhas.
+	 * Ele aplica automaticamente o "salting" e é recomendado para segurança de senhas em aplicações.
+	 * Este método retorna uma instância de BCryptPasswordEncoder, que pode ser injetada em outros componentes
+	 * para realizar operações seguras de codificação e verificação de senhas.
+	 */
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$X0Xq9SbdD/7VBKJV2l7yC.zjH8p8l//27Zok4vh9wGfxV5rS3ORcC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$X0Xq9SbdD/7VBKJV2l7yC.zjH8p8l//27Zok4vh9wGfxV5rS3ORcC');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
